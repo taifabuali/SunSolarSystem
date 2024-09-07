3cabb747054b343df2e9c022880c197acc74e55b
taifabuali <153289795+taifabuali@users.noreply.github.com>
2024-09-07 07:43:17 +0300

    Update AtmosphereValus.cs

--- Assets/Code/AtmosphereValus.cs
+++ Assets/Code/AtmosphereValus.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class AtmosphereValus : MonoBehaviour
 {
-    // Start is called before the first frame update
-   public float atmosphereDensity = 1.2f; // Default value
-    public float scaleHeight = 8500f; // Default value
+   public float atmosphereDensity = 1.2f; 
+    public float scaleHeight = 8500f; 
     
     public float gravity = 9.81f;
 
